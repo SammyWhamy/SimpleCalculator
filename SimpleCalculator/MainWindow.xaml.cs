6c42e8b4903fa170cd324cab55509a726152178b
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2020-12-02 16:14:55 +0100

    Fix percentage operator. Remove non-working keybinds. Clean up code

--- SimpleCalculator/MainWindow.xaml.cs
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Text.RegularExpressions;
 
 namespace SimpleCalculator
 {
@@ -13,7 +14,7 @@ namespace SimpleCalculator
 
         private void UpdateText(TextBox textbox)
         {
-            if(textbox.Text == "Nothing put in yet")
+            if (textbox.Text == "Nothing put in yet" || textbox.Text == "0")
             {
                 textbox.Text = "";
             }
@@ -25,7 +26,15 @@ namespace SimpleCalculator
             {
                 expression = expression.Replace("×", "*");
                 expression = expression.Replace("÷", "/");
-                System.Diagnostics.Debug.WriteLine(expression);
+                Regex rx = new Regex(@"(?<percentage>[\d.]+)(%)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                MatchCollection matches = rx.Matches(expression);
+                Console.WriteLine(matches);
+                foreach (Match match in matches)
+                {
+                    GroupCollection groups = match.Groups;
+                    expression = expression.Replace(match.Value, (Double.Parse(groups["percentage"].Value) / 100).ToString()+"*");
+                }
+                Console.WriteLine(expression);
                 System.Data.DataTable table = new System.Data.DataTable();
                 return Convert.ToDouble(table.Compute(expression, String.Empty));
             } catch (Exception)
@@ -54,17 +63,68 @@ namespace SimpleCalculator
                 }
             }
         }
-
+        // Number Clicks
         private void NumberZero_Click(object sender, RoutedEventArgs e)
         {
             UpdateText(_instance.Input);
             _instance.Input.Text += "0";
         }
+        private void NumberOne_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "1";
+        }
+        private void NumberTwo_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "2";
+        }
+        private void NumberThree_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "3";
+        }
+        private void NumberFour_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "4";
+        }
+        private void NumberFive_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "5";
+        }
+        private void NumberSix_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "6";
+        }
+        private void NumberSeven_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "7";
+        }
+        private void NumberEight_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "8";
+        }
+        private void NumberNine_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "9";
+        }
 
+        // Operator Clicks
         private void Plus_Click(object sender, RoutedEventArgs e)
         {
             UpdateText(_instance.Input);
             _instance.Input.Text += "+";
+        }
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "-";
         }
 
         private void Decimal_Click(object sender, RoutedEventArgs e)
@@ -72,37 +132,6 @@ namespace SimpleCalculator
             UpdateText(_instance.Input);
             _instance.Input.Text += ".";
         }
-
-        private void Clear_Click(object sender, RoutedEventArgs e)
-        {
-            _instance.Input.Text = "Nothing put in yet";
-            _instance.Result.Text = "0";
-        }
-
-        private void Minus_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateText(_instance.Input);
-            _instance.Input.Text += "-";
-        }
-
-        private void NumberThree_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateText(_instance.Input);
-            _instance.Input.Text += "3";
-        }
-
-        private void NumberFour_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateText(_instance.Input);
-            _instance.Input.Text += "4";
-        }
-
-        private void NumberFive_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateText(_instance.Input);
-            _instance.Input.Text += "5";
-        }
-
         private void Divide_Click(object sender, RoutedEventArgs e)
         {
             UpdateText(_instance.Input);
@@ -114,7 +143,13 @@ namespace SimpleCalculator
             UpdateText(_instance.Input);
             _instance.Input.Text += "×";
         }
+        private void Percentage_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateText(_instance.Input);
+            _instance.Input.Text += "%";
+        }
 
+        // Other Clicks
         private void Backspace_Click(object sender, RoutedEventArgs e)
         {
             if(_instance.Input.Text != "Nothing put in yet" && _instance.Input.Text.Length > 0)
@@ -122,35 +157,10 @@ namespace SimpleCalculator
                 _instance.Input.Text = _instance.Input.Text.Remove(_instance.Input.Text.Length - 1);
             }
         }
-
-        private void NumberNine_Click(object sender, RoutedEventArgs e)
+        private void Clear_Click(object sender, RoutedEventArgs e)
         {
-            UpdateText(_instance.Input);
-            _instance.Input.Text += "9";
-        }
-
-        private void NumberEight_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateText(_instance.Input);
-            _instance.Input.Text += "8";
-        }
-
-        private void NumberSeven_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateText(_instance.Input);
-            _instance.Input.Text += "7";
-        }
-
-        private void NumberSix_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateText(_instance.Input);
-            _instance.Input.Text += "6";
-        }
-
-        private void Modulus_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateText(_instance.Input);
-            _instance.Input.Text += "%";
+            _instance.Input.Text = "Nothing put in yet";
+            _instance.Result.Text = "0";
         }
     }
 }
